b60ca00c99fd0cf326bb4d044e51aaf54c9804b0
gognik <gognik07@gmail.com>
2019-04-11 21:04:02 +0300

    Tasks from lesson 1

---
+++ src/lesson1/tasks6/Main.java
@@ -0,0 +1,18 @@
+package lesson1.tasks6;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        DateFormat format = new SimpleDateFormat("hh:mm a MM/dd/yyyy", Locale.ENGLISH);
+        Date date = new Date();
+        System.out.println("English format: " + format.format(date));
+
+        format = new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH);
+        date = new Date();
+        System.out.println("Russian format: " + format.format(date));
+    }
+}
